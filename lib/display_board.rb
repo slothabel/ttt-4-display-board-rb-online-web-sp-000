108fe475be062ac7a43905aad1a1bbe9d6e6d3f8
Yoshi Mejia <yoshimejia00@gmail.com>
2020-09-10 15:22:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   |  X |   "
   puts "-----------"
   puts "   |   |   "
 end
