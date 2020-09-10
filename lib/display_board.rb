0e4b4dbb90990344452f9e25e344300950965400
Yoshi Mejia <yoshimejia00@gmail.com>
2020-09-10 15:25:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
 
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |  X |   "
+  puts "   | #{board[4]}  |   "
   puts "-----------"
   puts "   |   |   "
 end
